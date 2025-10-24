9d17e1c07d627edf14f3f12e49b9849af6b9c4c5
iceage889 <ejidike48@gamil.com>
2025-10-24 12:07:10 +0200

    added more comment

--- src/app/components/review.tsx
+++ src/app/components/review.tsx
@@ -9,7 +9,7 @@ const testimonials = [
     src: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    quote: "Reliable and Fast .",
+    quote: "Reliable,Fast and Keep to time  .",
     name: "Michael Rodriguez",
     designation: "",
     src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
